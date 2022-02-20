07048522858e2ceadd3747832179e5c8bcc6082f
Woosang <yws1502@naver.com>
2022-02-20 11:07:31 +0900

    🩹 날짜 포맷 변경

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem({ title, amount, date }) {
+  const Year = date.getFullYear();
+  const Month = date.toLocaleString("en-US", { month: "long" });
+  const Day = date.toLocaleString("en-US", { day: "2-digit" });
+
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <div>
+        <div>{Month}</div>
+        <div>{Year}</div>
+        <div>{Day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
