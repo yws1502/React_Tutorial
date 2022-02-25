638e19c0f433e8a5aa39391326aeae4dc94f65ef
Woosang <yws1502@naver.com>
2022-02-25 22:34:42 +0900

    ✨ Form component show, cancel 기능구현

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 function NewExpense(props) {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const EditingHandler = () => {
+    setIsEditing((prevShowState) => {
+      return prevShowState ? false : true;
+    });
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing ? (
+        <button type="button" onClick={EditingHandler}>
+          Add New Expense
+        </button>
+      ) : (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={EditingHandler}
+        />
+      )}
     </div>
   );
 }
